8dcf0574b1ba5d57a3645956646b4930376c005d
Vicente Hinostroza <vicente.azort@gmail.com>
2026-07-08 14:04:52 -0400

    Add pluginManagement and dependencyResolutionManagement to settings.gradle.kts This resolves the Android Gradle Plugin from google() maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://developer.huawei.com/repo/") }
+    }
+}
+
 rootProject.name = "godot-huawei-iap"
 include(":plugin")
